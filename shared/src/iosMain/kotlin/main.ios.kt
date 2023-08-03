ea8f955b3a565d2c2b8ae59d7e1467efc3d57800
sumit <sumit22mittal@gmail.com>
2023-08-03 12:00:00 -0700

    kotlin dispatche providers for ios and android + ktor impl

---
+++ shared/src/iosMain/kotlin/main.ios.kt
@@ -0,0 +1,8 @@
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController {
+    CompositionLocalProvider(){
+        App(root)
+    }
+}
