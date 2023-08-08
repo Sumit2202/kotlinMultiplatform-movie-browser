68399a115f864eef80472fc25585a2a08ad81855
sumit <sumit22mittal@gmail.com>
2023-08-08 12:00:00 -0700

    updated colors

--- shared/src/commonMain/kotlin/ui/features/detailsUi.kt
+++ shared/src/commonMain/kotlin/ui/features/detailsUi.kt
@@ -144,7 +144,7 @@ fun ShowKeyWords(keywords: List<String>) {
             Text(
                 fontSize = 8.sp,
                 text = word,
-                color = Color.White,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
                 modifier = Modifier
                     .background(color = MaterialTheme.colorScheme.primaryContainer, shape = CircleShape)
                     .padding(vertical = 2.dp, horizontal = 5.dp)
@@ -158,10 +158,10 @@ fun ShowMovieDetails(movieDetailsModel: MovieDetailsModel) {
     ShowImages(movieDetailsModel)
     ShowOverview(movieDetailsModel.overview, movieDetailsModel.vote)
     ShowMovieInfo(movieDetailsModel.movieInfo)
+    ShowKeyWords(movieDetailsModel.keywords)
     if (movieDetailsModel.reviews.isNotEmpty()) {
         ShowReviews(movieDetailsModel.reviews)
     }
-    ShowKeyWords(movieDetailsModel.keywords)
     ShowCasts(movieDetailsModel.topCast)
     ShowPostersAndBackDrops(movieDetailsModel.posters, movieDetailsModel.backdrops)
 
@@ -274,13 +274,13 @@ fun ShowReviewCards(review: MovieReview) {
                     Row(
                         modifier = Modifier
                             .padding(start = 7.dp)
-                            .background(Color.Blue, RoundedCornerShape(10.dp))
+                            .background(MaterialTheme.colorScheme.secondary, RoundedCornerShape(10.dp))
                             .padding(2.dp),
                         verticalAlignment = Alignment.CenterVertically
                     ) {
                         Text(
                             text = review.rating.toString(),
-                            color = Color.White,
+                            color = MaterialTheme.colorScheme.onSecondary,
                             fontSize = 12.sp
                         )
                         Image(
@@ -365,6 +365,7 @@ private fun ShowOverview(overview: String, vote: Double) {
 
     Text(
         text = annotatedString,
+        color = MaterialTheme.colorScheme.onBackground,
         inlineContent = inlineContentMap,
         modifier = Modifier
             .fillMaxWidth()
